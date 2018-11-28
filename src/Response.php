8bc56eba8b1f5195303726953dad773a1cf33e65
Mihai Blebea <mihaiserban.blebea@gmail.com>
2018-11-28 15:17:53 +0000

    remove pretty json from response

--- src/Response.php
+++ src/Response.php
@@ -7,8 +7,24 @@ use Interceptor\Interfaces\ResponseInterface;
 
 class Response implements ResponseInterface
 {
+    public static function code(Int $code = 200)
+    {
+        http_response_code($code);
+        return new static;
+    }
+
     public static function asJson($response)
     {
+        echo json_encode($users);
+    }
+
+    public static function asNiceJson()
+    {
         echo '<pre>'; echo json_encode($response, JSON_PRETTY_PRINT); echo '</pre>';
+    }
+
+    public static function asFile(String $file_path)
+    {
+        return readfile($file_path);
     }
 }
